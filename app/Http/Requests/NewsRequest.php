cfc820b3e7871d6d27eb476098989c39880c64e8
MustaphaRam <mustapha.mad.75@gmail.com>
2024-04-25 17:03:03 +0100

    Requests commit

---
+++ app/Http/Requests/NewsRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class NewsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'titre' => ['required', 'string', 'max:255', 'min:3'],
+            'contenu' => ['required', 'string', 'max:10000'],
+            'categorie' => ['required', 'exists:categories,name'],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'error' => true,
+            'message' => 'Error de validation',
+            'errorList' => $validator->errors()
+        ],400));
+    }
+}
